c0206f8b94ab66c3015bb1c3e644d582abc164f8
Axel Pulis <a100872@mcast.org.mt>
2017-11-17 14:07:50 +0100

    17/11/2017

---
+++ Brick Breaker 4.2D/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddle;
+    Vector3 paddleBallDiff;
+    bool gameStart = false;
+
+	// Use this for initialization
+	void Start () {
+        paddleBallDiff = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+
+        if (!gameStart)
+        {
+            this.transform.position = paddle.transform.position + paddleBallDiff;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !gameStart)
+        {
+            gameStart = true;
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+        }
+	}
+}
---
+++ Brick Breaker 4.2D/Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float mouseInBlocks = (Input.mousePosition.x / Screen.width * 16) -8;
+        Vector3 newPaddlePos = new Vector3(mouseInBlocks, gameObject.transform.position.y, gameObject.transform.position.z);
+        newPaddlePos.x = Mathf.Clamp(mouseInBlocks, -7.36f, 7.36f);
+        gameObject.transform.position = newPaddlePos;
+
+        
+    }
+}
