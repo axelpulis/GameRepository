ed4e8f667bcd503db92a7873244c3c9a397a17fe
axelpulis <axel.pulis.a100872@mcast.edu.mt>
2017-12-12 21:18:57 +0100

    -Created the Unity project -Created the MenuScreen -Play button implemented -Quit button implemented -Level Manager script was created to make the buttons functional

---
+++ AxelPulis_4.2D/AxelPulis_4.2D/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadNewScene(string sceneName)
+    {
+        
+        SceneManager.LoadScene(sceneName);
+        
+    }
+
+    public void QuitUnity()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void LoadNextScene()
+    {
+        
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        currentScene++;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    public int getScene()
+    {
+        int y = SceneManager.GetActiveScene().buildIndex;
+        return y;
+    }
+}
