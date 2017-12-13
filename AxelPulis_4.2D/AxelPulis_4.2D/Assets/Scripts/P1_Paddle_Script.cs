866efbd2994a9046afa53b4f1836b0c68ac4be28
axelpulis <axel.pulis.a100872@mcast.edu.mt>
2017-12-13 17:59:31 +0100

    -Created level1 scene -Added background -Added paddles for both players -Added script to control both paddles (Keyboard/Mouse)

---
+++ AxelPulis_4.2D/AxelPulis_4.2D/Assets/Scripts/P1_Paddle_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1_Paddle_Script : MonoBehaviour {
+
+    private Rigidbody2D rbPaddle1;
+
+    // Use this for initialization
+	void Start () {
+
+        rbPaddle1 = this.GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rbPaddle1.velocity = new Vector2(0f, 6f);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rbPaddle1.velocity = new Vector2(0f, -6f);
+        }
+        else
+        {
+            rbPaddle1.velocity = new Vector2(0f, 0f);
+        }
+		
+
+    }
+}
---
+++ AxelPulis_4.2D/AxelPulis_4.2D/Assets/Scripts/P2_Paddle_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2_Paddle_Script : MonoBehaviour {
+    private Rigidbody2D rbPaddle2;
+    // Use this for initialization
+    void Start()
+    {
+
+        rbPaddle2 = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            rbPaddle2.velocity = new Vector2(0f, 6f);
+        }
+        else if (Input.GetKey(KeyCode.Mouse1))
+        {
+            rbPaddle2.velocity = new Vector2(0f, -6f);
+        }
+        else
+        {
+            rbPaddle2.velocity = new Vector2(0f, 0f);
+        }
+
+
+    }
+}
